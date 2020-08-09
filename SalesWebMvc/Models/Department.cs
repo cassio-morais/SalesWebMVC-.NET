78dbe4289fefe63ca47b0a0d2d0ffc66ee9dea8a
cassio-morais <cassiomoraisg@hotmail.com>
2020-08-08 22:56:00 -0300

    DELETANDO ALTERAÇÕES commit:60f91f1 = Controller > action > viewdata > views > 'controller/action/'

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -5,7 +5,5 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        // classe básica do departamento associado a um DepartmentsController criado em Controllers
-        // pasta Controller > botão direito > add > controller > MVC controller empty > DepartmentsController
     }
 }
