0b39a9328a2835e77df99fa8eeb6831452b435ef
cassio-morais <cassiomoraisg@hotmail.com>
2020-08-09 02:46:41 -0300

    + entidades, regras de negócio e novas migrations

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,43 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        // Icollection: colecao generica que engloba listas, hashset... 
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            var total = Sellers
+                .Sum(seller => seller.TotalSales(initial, final));
+            // reaproveitando o total de vendas de cada vendedor
+
+            return total;
+        } 
+    
     }
 }
