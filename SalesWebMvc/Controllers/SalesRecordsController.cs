70672a8aa5f1eb12973b52c31c3cef450e5414b6
cassio-morais <cassiomoraisg@hotmail.com>
2020-08-19 19:13:36 -0300

    Edit, delete, details para Sales, excluido: HomeController

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.Enums;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -13,12 +16,17 @@ namespace SalesWebMvc.Controllers
 
         private readonly SalesRecordService _salesRecordService;
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SalesRecordsController(SalesRecordService salesRecordService, SellerService sellerService)
+        public SalesRecordsController(SalesRecordService salesRecordService,
+                                        SellerService sellerService,
+                                        DepartmentService departmentService)
         {
             _salesRecordService = salesRecordService;
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
+
 
         public IActionResult Index()
         {
@@ -52,6 +60,120 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+
+
+        public async Task<IActionResult> EditSale(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "id mismatch" });
+            }
+
+            var obj = await _salesRecordService.FindByIdAsync(id.Value);
+
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "Sale Not Found" });
+            }
+
+            ViewBag.Status = new SelectList(Enum.GetValues(typeof(SalesStatus)));
+            ViewBag.Sellers = new SelectList(await _sellerService.FindAllAsync(), "Id", "Name");
+
+            return View(obj);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSale(int id, SalesRecord obj)
+        {
+            if (id != obj.Id)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "id mismatch" });
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Status = new SelectList(Enum.GetValues(typeof(SalesStatus)), obj.Status);
+                ViewBag.Sellers = new SelectList(await _sellerService.FindAllAsync(), "Id", "Name", obj.Seller);
+                return View(obj);
+            }
+
+            try
+            {
+                await _salesRecordService.UpdateAsync(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException e)
+            {
+                return View(nameof(Error), new { Message = e.Message });
+            }
+
+
+        }
+
+
+        public async Task<IActionResult> DetailsSale(int? id)
+        {
+
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "id mismatch" });
+            }
+
+            var obj = await _salesRecordService.FindByIdAsync(id.Value);
+
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "Sale Not Found" });
+            }
+
+            ViewBag.Seller = await _sellerService.FindByIdAsync(obj.SellerId);
+            ViewBag.Department = await _departmentService.FindByIdAsync(obj.Seller.DepartmentId);
+
+            return View(obj);
+        }
+
+
+
+        public async Task<IActionResult> DeleteSale(int? id)
+        {
+
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "Id Mismatch" });
+            }
+
+            var obj = await _salesRecordService.FindByIdAsync(id.Value);
+
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { Message = "Sale Not Found" });
+            }
+
+            ViewBag.Seller = await _sellerService.FindByIdAsync(obj.SellerId);
+            ViewBag.Department = await _departmentService.FindByIdAsync(obj.Seller.DepartmentId);
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteSale(int id)
+        {
+            try { 
+                await _salesRecordService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            } 
+            catch(ApplicationException e)
+            {
+                return View(nameof(Error), new { e.Message });
+            }
+
+        }
+
 
 
 
@@ -94,6 +216,19 @@ namespace SalesWebMvc.Controllers
             var result = await _salesRecordService.FindByDateGroupingAsync(minDate.Value, maxDate.Value);
 
             return View(result);
+        }
+
+
+        public IActionResult Error(string message) // erro não se torna assíncrono pq, nesse caso, ele não faz nenhuma operação no banco
+        {
+            var errorviewmodel = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                Message = message
+            };
+
+            return View(errorviewmodel);
+
         }
 
 
